54e3c84a17709149b1901c7cc6dbe5428ccb36fe
Sanchit <sanchit0496@gmail.com>
2021-06-28 23:59:56 +0530

    actions update react redux

--- todolist/src/components/Todo.js
+++ todolist/src/components/Todo.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react' 
+import { addTodo, deleteTodo, removeTodo} from '../actions/index'
 import './Todo.css'
 
 const Todo = () => {
+    const [inputData, setInputData] = useState('')
     return (
         <div>
             <div className="main-div">
@@ -10,7 +12,10 @@ const Todo = () => {
                         <figcaption>Add your list here</figcaption>
                     </figure>
                     <div className="addItems">
-                        <input type="text" placeholder = "Add Items"/>
+                        <input type="text" placeholder = "Add Items"
+                        value = {inputData}
+                        onChange = {(event) => setInputData(event.target.value)}
+                        />
                         <i className="fa fa-plus add-btn"></i>
                     </div>
                 </div>
